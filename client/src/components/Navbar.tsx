91ff097230929fef43bc8b26116ba09486c8b9ac
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-06-27 23:51:47 +0300

    added styling to navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -7,44 +7,45 @@ import ListAltIcon from "@mui/icons-material/ListAlt";
 import GroupsIcon from "@mui/icons-material/Groups";
 import SettingsIcon from "@mui/icons-material/Settings";
 
-import "../images/reactronlogo.PNG";
+import avatar from "../images/logo.svg";
+import logo from "../images/reactronlogo.png";
 
 const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="user-card">
-        <img src="../images/reactronlogo.PNG" alt="Reactron logo" />
+        <img src={avatar} alt="Reactron logo" />
         <p>Welcome Back!</p>
         <p className="user-role">Admin</p>
       </div>
-      <div>
-        <Link to="/">
-          <img src="../images/reactronlogo.PNG" alt="Reactron logo" />
+      <div className="nav-links">
+        <Link to="/" className="navlink">
+          <img src={logo} alt="Reactron logo" />
           Dashboard
         </Link>
-        <Link to="/calendar">
+        <Link to="/calendar" className="navlink">
           <CalendarMonthIcon /> Calendar
         </Link>
-        <Link to="/users">
+        <Link to="/users" className="navlink">
           <GroupIcon /> Users
         </Link>
-        <Link to="/sickleavesholidays">
+        <Link to="/sickleavesholidays" className="navlink">
           <ListAltIcon />
           Sick leaves/holidays
         </Link>
-        <Link to="/customers">
+        <Link to="/customers" className="navlink">
           <GroupsIcon />
           Customers
         </Link>
-        <Link to="/settings">
+        <Link to="/settings" className="navlink">
           <SettingsIcon />
           Settings
         </Link>
-        <button className="logout-btn">
-          <MeetingRoomIcon />
-          Logout
-        </button>
       </div>
+      <button className="logout-btn">
+        <MeetingRoomIcon />
+        Logout
+      </button>
     </div>
   );
 };
