0527274906198066b6f4d57d78ac13629c08c312
David Amebley <davameb@gmail.com>
2022-06-28 10:28:38 +0300

    Create Initial Report table

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Reports from '../../components/Reports'
 
 export default function Home() {
 
   return (
     <>
       <p>Here we go!</p>
+      <Reports />
     </>
   )
 }
