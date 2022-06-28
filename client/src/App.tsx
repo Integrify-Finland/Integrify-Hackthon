5895f39d279558b88e038d452307984a26b8c951
David Amebley <davameb@gmail.com>
2022-06-28 18:26:06 +0300

    Create separate folders for pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,12 @@ import Navbar from "./components/Navbar";
 import Calendar from "./pages/Calendar";
 import Customers from "./pages/Customers";
 import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import SickLeavesHolidays from "./pages/SickLeavesHolidays";
 // import sickLeavesHolidays from "./pages/sickLeavesHolidays";
 // import Users from "./pages/Users";
 
 import Home from "./pages/Home";
-import CustomerList from "./components/CustomerList";
-import UserList from "./components/UserList";
-import LeavesAndHolidays from "./components/LeavesAndHolidays";
 
 function App() {
   return (
@@ -19,9 +18,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="customers" element={<CustomerList />} />
-        <Route path="users" element={<UserList />} />
-        <Route path="sickleavesholidays" element={<LeavesAndHolidays />} />
+        <Route path="customers" element={< Customers/>} />
+        <Route path="users" element={<Users />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="calendar" element={<Calendar />} />
+        <Route path="sickleavesholidays" element={<SickLeavesHolidays />} />
       </Routes>
     </div>
   );
---
+++ client/src/pages/SickLeavesHolidays/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LeavesAndHolidays from "../../components/LeavesAndHolidays";
+
+const sickLeavesHolidays = () => {
+  return <>
+          <LeavesAndHolidays/>
+        </>;
+};
+
+export default sickLeavesHolidays;
