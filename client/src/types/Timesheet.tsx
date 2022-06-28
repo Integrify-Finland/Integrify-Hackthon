9d4457fef17ec6a1f4fb7a676472ba9d2fffa60e
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-06-28 21:02:47 +0300

    added types

---
+++ client/src/types/Timesheet.tsx
@@ -0,0 +1,26 @@
+[
+  {
+    rate: 0,
+    _id: "62bb0ee4a82da43c7c19b791",
+    date: "2022-05-30T21:00:00.000Z",
+    from: "1970-01-01T00:00:00.020Z",
+    to: "1970-01-01T00:00:00.000Z",
+    duration: 14820,
+    userName: "62babcedb346744694633801",
+    customerName: "62bac709d2769b2484a0866a",
+    exported: "No",
+    __v: 0,
+  },
+  {
+    rate: 0,
+    _id: "62bb0ef8a82da43c7c19b793",
+    date: "2022-05-30T21:00:00.000Z",
+    from: "1970-01-01T00:00:00.016Z",
+    to: "1970-01-01T00:00:00.018Z",
+    duration: 5400,
+    userName: "62babca4a132f22c149a0525",
+    customerName: "62bacc1ff5ef3e2a101f2e3f",
+    exported: "No",
+    __v: 0,
+  },
+];
---
+++ client/src/types/User.tsx
@@ -0,0 +1,14 @@
+[
+  {
+    _id: "62babca4a132f22c149a0525",
+    fullName: "Anders Svartholm",
+    internalRate: 25.67,
+    __v: 0,
+  },
+  {
+    _id: "62babcedb346744694633801",
+    fullName: "Aaro Leppänen",
+    internalRate: 38.67,
+    __v: 0,
+  },
+];
