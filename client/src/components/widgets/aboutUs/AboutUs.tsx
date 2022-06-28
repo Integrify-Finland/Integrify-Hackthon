671d8fbd4ef4a218bf73d987dd5072b7ec6ff7a2
Roman Kuzero <rombeldan@gmail.com>
2022-06-29 01:08:43 +0300

    change ui

--- client/src/components/widgets/aboutUs/AboutUs.tsx
+++ client/src/components/widgets/aboutUs/AboutUs.tsx
@@ -10,12 +10,12 @@ const AboutUs = () => {
         </Typography>
         <Button variant="contained">Explore</Button>
       </div>
-      {/* <div className="right">
-        <img
+       <div className="right">
+        {/* <img
           src="https://images.unsplash.com/photo-1560762484-813fc97650a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2874&q=80"
           alt="keyboard"
-        />
-      </div> */}
+        /> */}
+      </div> 
     </div>
   );
 };
