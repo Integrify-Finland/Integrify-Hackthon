c261f566faf829e6d0bdd68afd3578b696150d52
David Amebley <davameb@gmail.com>
2022-06-28 21:25:32 +0300

    Add interfaces for Customer, User, and Timesheet

--- client/src/types/User.tsx
+++ client/src/types/User.tsx
@@ -1,14 +1,21 @@
-[
-  {
-    _id: "62babca4a132f22c149a0525",
-    fullName: "Anders Svartholm",
-    internalRate: 25.67,
-    __v: 0,
-  },
-  {
-    _id: "62babcedb346744694633801",
-    fullName: "Aaro Leppänen",
-    internalRate: 38.67,
-    __v: 0,
-  },
-];
+export interface User{
+  _id: string,
+  fullName: string,
+  internalRate: number,
+  __v: number
+}
+
+// [
+//   {
+//     _id: "62babca4a132f22c149a0525",
+//     fullName: "Anders Svartholm",
+//     internalRate: 25.67,
+//     __v: 0,
+//   },
+//   {
+//     _id: "62babcedb346744694633801",
+//     fullName: "Aaro Leppänen",
+//     internalRate: 38.67,
+//     __v: 0,
+//   },
+// ];
